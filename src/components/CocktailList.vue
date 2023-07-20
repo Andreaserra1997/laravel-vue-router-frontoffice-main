3e6a9a4109e92eaeeb27f2773e632967b60a6011
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-07-20 17:23:41 +0200

    add category

--- src/components/CocktailList.vue
+++ src/components/CocktailList.vue
@@ -15,6 +15,7 @@ export default {
   data() {
     return {
       arrCocktails: [],
+      arrCategories: [],
     };
   },
   methods: {
@@ -23,14 +24,35 @@ export default {
         this.arrCocktails = response.data.results;
       });
     },
+    getCategories() {
+      axios.get("http://localhost:8000/api/categories").then((response) => {
+        this.arrCategories = response.data.results;
+      });
+    },
   },
   created() {
     this.getCocktails();
+    this.getCategories();
   },
 };
 </script>
 
 <template>
+  <form>
+    <h2>Filtra le Categorie</h2>
+
+    <label for="categories">Categorie</label>
+    <select class="form-select" id="categories">
+      <option
+        v-for="category in arrCocktails"
+        :key="category.id"
+        :value="category.id"
+      >
+        {{ category.strCategory }}
+      </option>
+    </select>
+  </form>
+
   <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4 mb-5">
     <div class="col" v-for="cocktail in arrCocktails" :key="cocktail.id">
       <CocktailCard :cocktail="cocktail" />
