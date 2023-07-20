6aa405a6831bf6376f27b2cba1c8f883474701a8
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-07-20 15:51:12 +0200

    first commit

---
+++ src/components/navBar.vue
@@ -0,0 +1,90 @@
+<script>
+export default {
+  data() {
+    return {
+      searchString: "",
+    };
+  },
+};
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+      <!-- <a class="navbar-brand" href="#"></a> -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link
+              :to="{ name: 'home' }"
+              class="nav-link active text-light"
+              aria-current="page"
+              >HOME</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              :to="{ name: 'cocktails.index' }"
+              class="nav-link text-light"
+              >COCKTAILS</router-link
+            >
+          </li>
+
+          <li class="nav-item">
+            <router-link :to="{ name: 'about' }" class="nav-link text-light"
+              >ABOUT</router-link
+            >
+          </li>
+
+          <li class="nav-item">
+            <router-link :to="{ name: 'contact' }" class="nav-link text-light"
+              >CONTACT US</router-link
+            >
+          </li>
+        </ul>
+
+        <div>
+          <img src="" alt="" />
+        </div>
+
+        <form
+          class="d-flex"
+          role="search"
+          @submit.prevent="
+            $router.push({
+              name: 'cocktails.index',
+              query: { q: searchString },
+            })
+          "
+        >
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            name="q"
+            v-model="searchString"
+          />
+          <button class="btn btn-light" type="submit">SEARCH</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  height: 100px;
+}
+</style>
---
+++ src/pages/AppAbout.vue
@@ -0,0 +1,26 @@
+<script></script>
+
+<template>
+  <div class="container">
+    <h1></h1>
+    <p>
+      Creare Cocktail è un'arte, si sa, e come ogni pittore possiede il suo
+      stile, ogni Mixologist può eseguire una ricetta cocktail secondo delle
+      varianti suggerite dal suo sapere, oltre che dal mestiere di barman. Si
+      scelgono dosi, strumenti e attrezzature bar adeguate, si studiano le
+      diluizioni e le interazioni tra distillati e bevande. Non creiamo semplici
+      cocktail ma esperienze sensoriali che coinvolgono tutti i nostri sensi,
+      dalla vista, attraverso l'estetica e il dettaglio dei nostri cocktail, al
+      gusto particolare, ricercato, raffinato che vi farà scoprire nuove
+      frontiere del sapore. Ubriacatevi di Arte!
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1100px;
+  padding: 0.5rem;
+  margin: 0 auto;
+}
+</style>
