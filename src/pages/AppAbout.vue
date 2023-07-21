efb26021c545684b85e8975ea605283e01116c8f
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-21 11:03:32 +0200

    about us style

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -17,11 +17,13 @@
       </p>
     </div>
 
-    <h3 class="text-white text-center">Our Team</h3>
+    <h3 class="text-center">Our Team</h3>
     <div class="row">
       <div class="column">
         <div class="card">
-          <img src="https://picsum.photos/200" alt="Gabriella Valentina">
+          <img
+            src="https://img.freepik.com/premium-vector/retro-70s-alcohol-cocktails-cartoon-style-nostalgia-80s-groovy-martini-glass-character-y2k_693602-533.jpg?w=1060"
+            alt="Gabriella Valentina">
           <div class="container">
             <h2>Gabriella Valentina</h2>
             <p class="title">CEO & Founder</p>
@@ -34,7 +36,9 @@
 
       <div class="column">
         <div class="card">
-          <img src="https://picsum.photos/200" alt="Gianluca Ligori">
+          <img
+            src="https://cdn.vectorstock.com/i/1000x1000/77/97/blue-lagoon-cocktail-cartoon-character-design-vector-36717797.webp"
+            alt="Gianluca Ligori">
           <div class="container">
             <h2>Gianluca Ligori</h2>
             <p class="title">Art Director</p>
@@ -47,7 +51,9 @@
 
       <div class="column">
         <div class="card">
-          <img src="https://picsum.photos/200" alt="Andrea Serra">
+          <img
+            src="https://cdn.vectorstock.com/i/1000x1000/70/33/a-cartoon-drawing-mai-tai-cocktail-in-super-vector-36717033.webp"
+            alt="Andrea Serra">
           <div class="container">
             <h2>Andrea Serra</h2>
             <p class="title">Art Director</p>
@@ -60,7 +66,9 @@
 
       <div class="column">
         <div class="card">
-          <img src="https://picsum.photos/200" alt="Matteo Mancuso">
+          <img
+            src="https://cdn4.vectorstock.com/i/1000x1000/79/43/wink-margarita-character-cartoon-style-vector-20767943.jpg"
+            alt="Matteo Mancuso">
           <div class="container">
             <h2>Matteo Mancuso</h2>
             <p class="title">Art Director</p>
@@ -73,7 +81,9 @@
 
       <div class="column">
         <div class="card">
-          <img src="https://picsum.photos/200" alt="Gabriele Barcellona">
+          <img
+            src="https://img.myloview.com/stickers/a-wealthy-bloody-mary-cocktail-cartoon-character-with-much-money-700-209043758.jpg"
+            alt="Gabriele Barcellona">
           <div class="container">
             <h2>Gabriele Barcellona</h2>
             <p class="title">Designer</p>
@@ -88,28 +98,48 @@
 </template>
 
 <style lang="scss" scoped>
+body {
+  background: var(--bg);
+
+  /*COLORS*/
+  --bg: #738376;
+  --firstColor: #59583a;
+  --secondColor: #b54426;
+  --thirdColor: #dcb06f;
+  --button: #dcb06f;
+
+  /*FONTS*/
+  --mono: 'source-code-pro';
+  --display: 'lust-sans';
+}
+
 h2 {
   font-size: 1.3rem;
+  margin-top: 1rem;
 }
+
+h3 {
+  color: var(--thirdColor);
+}
+
+
 
 .column {
   width: 20%;
-  padding: 0 8px;
+  padding: 0 5px;
 }
 
 .card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  margin: 8px;
+  margin: .5rem;
+  background-color: var(--thirdColor);
 }
 
 .about-section {
   text-align: center;
-  color: white;
+  color: var(--button);
 }
 
-.container {
-  padding: 0 16px;
-}
+
 
 .container::after,
 .row::after {
@@ -119,22 +149,22 @@ h2 {
 }
 
 .title {
-  color: grey;
+  color: var(--firstColor);
 }
 
 .button {
   border: none;
   display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
+  padding: .5rem;
+  color: var(--thirdColor);
+  background-color: var(--firstColor);
   text-align: center;
   cursor: pointer;
   width: 100%;
 }
 
 .button:hover {
-  background-color: #555;
+  background-color: var(--secondColor);
 }
 
 @media screen and (max-width: 650px) {
