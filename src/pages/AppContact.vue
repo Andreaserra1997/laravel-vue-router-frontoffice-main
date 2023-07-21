07cd8f617dd33661f0a68cee03a5308b522d5ab5
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-07-21 10:48:45 +0200

    edit contact

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -7,22 +7,40 @@ export default {};
   <form>
     <div class="mb-3" novalidate>
       <label for="name" class="form-label">Name</label>
-      <input type="text" class="form-control" id="name" aria-describedby="emailHelp" />
+      <input
+        type="text"
+        class="form-control"
+        id="name"
+        aria-describedby="emailHelp"
+      />
     </div>
 
     <div class="mb-3">
       <label for="email" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="email" aria-describedby="emailHelp" />
+      <input
+        type="email"
+        class="form-control"
+        id="email"
+        aria-describedby="emailHelp"
+      />
     </div>
 
     <div class="mb-3">
-      <label for="exampleFormControlTextarea1" class="form-label">Message</label>
-      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <label for="exampleFormControlTextarea1" class="form-label"
+        >Message</label
+      >
+      <textarea
+        class="form-control"
+        id="exampleFormControlTextarea1"
+        rows="3"
+      ></textarea>
     </div>
 
     <div class="mb-3 form-check">
       <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-      <label class="form-check-label" for="exampleCheck1">subscribe to the NewsLetters</label>
+      <label class="form-check-label" for="exampleCheck1"
+        >subscribe to the NewsLetters</label
+      >
     </div>
 
     <button type="submit" class="btn btn-primary">Submit</button>
