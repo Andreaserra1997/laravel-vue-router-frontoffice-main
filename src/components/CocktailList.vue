54a6ce45214a7f9b6c2a585a3daabc8f58b63837
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-07-21 12:44:33 +0200

    edit filter list

--- src/components/CocktailList.vue
+++ src/components/CocktailList.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from "axios";
 import CocktailCard from "./CocktailCard.vue";
+import CocktailsFilter from "./CocktailsFilter.vue";
 import { store } from "../store";
 
 export default {
@@ -9,21 +10,32 @@ export default {
       store,
       arrCocktails: [],
       arrCategories: [],
+      categoryId: null,
     };
   },
   components: {
     CocktailCard,
+    CocktailsFilter,
   },
   methods: {
     getCocktails() {
-      axios.get(this.store.baseUrl + "api/cocktails").then((response) => {
-        this.arrCocktails = response.data.results;
-      });
+      axios
+        .get(this.store.baseUrl + "api/cocktails", {
+          params: { category: this.categoryId },
+        })
+        .then((response) => {
+          this.arrCocktails = response.data.results;
+        });
     },
     getCategories() {
       axios.get(this.store.baseUrl + "api/category").then((response) => {
         this.arrCategories = response.data;
+        console.log(this.arrCategories);
       });
+    },
+    manageChangeCategory(categoryId) {
+      this.categoryId = categoryId;
+      this.getCocktails();
     },
   },
   created() {
@@ -34,21 +46,10 @@ export default {
 </script>
 
 <template>
-  <form>
-    <label for="categories">
-      <h2>Filtra i cocktail per categoria:</h2>
-    </label>
-    <select class="form-select" id="categories">
-      <option
-        v-for="category in arrCategories"
-        :key="category.id"
-        :value="category.id"
-      >
-        {{ category.strCategory }}
-      </option>
-    </select>
-  </form>
-
+  <CocktailsFilter
+    :categories="arrCategories"
+    @changeCategory="manageChangeCategory($event)"
+  />
   <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4 mb-5">
     <div class="col" v-for="cocktail in arrCocktails" :key="cocktail.id">
       <CocktailCard :cocktail="cocktail" />
